cbbf1bd33d5c687781bf00c4b8e304407ffd7f7b
batu-el <bel@tulane.edu>
2024-09-22 22:06:03 -0700

    set up venv

---
+++ run.py
@@ -0,0 +1,11 @@
+# gets the data from data folder
+# gets the code from library
+# makes computations
+# saves the results in res
+from library.data_generation.generate import generate_new_dataset
+
+# Run the function
+if __name__ == "__main__":
+    source_dataset_folder = "data/nlp4lp"
+    target_dataset_folder = "data/test"
+    generate_new_dataset(source_dataset_folder, target_dataset_folder)
